4e05d128f8b4c5f247d7f0126dd9d683f32c3e31
orange0227jas <74972149+orange0227jas@users.noreply.github.com>
2020-12-01 23:23:50 +0900

    stylecss-split

--- src/components/inputTodo.jsx
+++ src/components/inputTodo.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 
+const style = {
+  backgroundColor: "aqua",
+  width: "400px",
+  height: "30px",
+  padding: "8px",
+  margin: "8px",
+  borderRadius: "10px"
+};
+
 export const InputTodo = (props) => {
   const { todoText, onChange, onClick } = props;
   return (
-    <div className="input-area">
+    <div style={style}>
       <input
         placeholder="ここに入力〜"
         value={todoText}
